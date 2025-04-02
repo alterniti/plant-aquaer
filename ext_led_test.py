e40a85ec4ff467727c4a6304d256b0181a7bc68b
alterniti <elijahross695@gmail.com>
2025-04-02 15:12:48 -0400

    working led test

--- ext_led_test.py
+++ ext_led_test.py
@@ -2,11 +2,12 @@ from machine import Pin
 from time import sleep
 
 print("hello?")
-led=Pin(8, Pin.OUT)
+led=Pin(15, Pin.OUT)
 led2=Pin("LED", Pin.OUT)
 
-
-# led.value(not led.value())
-# led2.value(not led.value())
-# sleep(0.5)
-# print(led.value)
+while True:
+    led.value(not led.value())
+    led2.value(not led.value())
+    sleep(0.5)
+    print(f"{led.value()}, {led2.value()}")
+    print(led)
