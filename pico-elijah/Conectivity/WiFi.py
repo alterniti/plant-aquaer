0a297efbde3c4243028415d3412c1fc4f3f25b5a
22518 <22518@students.gcsc.k12.in.us>
2025-04-04 15:24:10 -0400

    WiFi.py

---
+++ pico-elijah/Conectivity/WiFi.py
@@ -0,0 +1,19 @@
+import network
+import time
+
+# WiFi credentials
+ssid = "Eli-comp"
+password = "pico-2-w@s"
+
+# Initialize the WiFi interface
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+
+# Connect to the WiFi network
+wlan.connect(ssid, password)
+
+# Wait for the connection to establish
+while not wlan.isconnected():
+    print("Connecting to network...")
+    time.sleep(1)
+    
