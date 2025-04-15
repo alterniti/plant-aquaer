1276d8b5e40919c18231b019dc4739f51b7d6bff
alterniti <elijahross695@gmail.com>
2025-04-15 14:13:09 -0400

    added net credentials json

--- ext_led_test.py
+++ ext_led_test.py
@@ -1,13 +1,9 @@
 from machine import Pin
 from time import sleep
 
-print("hello?")
-led=Pin(15, Pin.OUT)
+
 led2=Pin("LED", Pin.OUT)
 
 while True:
-    led.value(not led.value())
-    led2.value(not led.value())
+    led2.value(not led2.value())
     sleep(0.5)
-    print(f"{led.value()}, {led2.value()}")
-    print(led)
