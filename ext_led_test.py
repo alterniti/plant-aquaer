04f0d17ce19e25811bc5f40641b09aa7015c901d
alterniti <elijahross695@gmail.com>
2025-04-16 13:59:37 -0400

    main file network config

--- ext_led_test.py
+++
@@ -1,9 +0,0 @@
-from machine import Pin
-from time import sleep
-
-
-led2=Pin("LED", Pin.OUT)
-
-while True:
-    led2.value(not led2.value())
-    sleep(0.5)
--- pico-elijah/main.py
+++ pico-elijah/main.py
@@ -1,6 +1,7 @@
 # this will be the main file. 
 
 # ↓↓↓ added 'type: ignore' to prevent 'module not found' errors ↓↓↓
+import machine #type: ignore
 from machine import Pin #type: ignore
 import network #type: ignore
 import socket #type: ignore
@@ -12,36 +13,54 @@ class connect:
     net_creds={
         "elija-school-pc":{
             "ssid":"Eli-comp",
-            "pass":"pico-2-w@s"
+            "passw":"pico-2-w@s"
         },
         "brandon-school-pc":{
-            "ssid":"",
-            "pass":""
+            "ssid":"None",
+            "passw":"None"
         },
         "joseph-iphone":{
             "ssid":"Joseph's iPhone",
-            "pass":"Please!!"
+            "passw":"Please!!"
         },
         "elijah-laptop":{
-            "ssid":"",
-            "pass":""
+            "ssid":"None",
+            "passw":"None"
         }
     }
 
-    def __init__():
-        pass
+    def __init__(self):
+        # initializes network adapter
+        self.wlan=network.WLAN(network.STA_IF)
 
-    def connect():
-        pass
+    def list_networks(self):
+        print(self.wlan.scan())
 
-    def ping(server_ip,password,data):
+    def connect(self, ssid):
+        print(f"Available networks:")
+        nets_a=self.wlan.scan()
+        for net in nets_a:
+            print(net)
+        for saved_net in connect.net_creds.items():
+            if saved_net['ssid'] in nets_a:
+                print("connecting to {saved_net['ssid']}...")
+                try:
+                    self.wlan.connect(saved_net['ssid'],saved_net['passw'])
+                    print("connected")
+                except:
+                    print("connection failed")
+
+                
+        
+
+    def ping(self,server_ip,password,data):
         pass
 
 class pump:
-    def __init__():
-        pass
+    def __init__(self, pin: int):
+        return f"pump{pin}"
 
-    def pump_water(duration):
+    def pump_water(self, duration):
         pass
 
 class sensors:
@@ -49,19 +68,19 @@ class sensors:
         # params:
         # type:: MOISTURE or FLOW
         # pin:: GPID
-        name=type.join()
-        return 
+        name=type.join(str(pin))
+        return name
 
-    def get_moisture():
+    def get_moisture(self):
         pass
-    def is_moist():
+    def is_moist(self):
         pass
 
     def get_flow(self):
         pass
 
-class main:
-    def init():
-        print("Plant Aquaer\nA project for PLTW's EDD course\n\nBy Gatlin Meyer, Brandon Peterson, and Elijah Ross")
 
-main.init()
+def startup():
+    print("Plant Aquaer\nA project for PLTW's EDD course\n\nBy Gatlin Meyer, Brandon Peterson, and Elijah Ross")
+
+connect().list_networks()
